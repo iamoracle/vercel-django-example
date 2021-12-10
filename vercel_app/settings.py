58bdd72f13feb27c2fe714c2cf4ed5d52b53570f
iamoracle <38940247+iamoracle@users.noreply.github.com>
2021-12-10 22:47:37 +0100

    added cors to allow all origin

--- vercel_app/settings.py
+++ vercel_app/settings.py
@@ -30,6 +30,8 @@ ALLOWED_HOSTS = [
     '.vercel.app'
 ]
 
+CORS_ALLOW_ALL_ORIGINS = True # If this is used then `CORS_ALLOWED_ORIGINS` will not have any effect
+CORS_ALLOW_CREDENTIALS = True
 
 # Application definition
 
