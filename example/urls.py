1632d8f448115d6413662ab81039e6f40b6f43be
iamoracle <38940247+iamoracle@users.noreply.github.com>
2021-12-12 12:04:38 +0100

    added transactions to urls

--- example/urls.py
+++ example/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from example.views import index, price
+from example.views import index, price, transactions
 
 
 urlpatterns = [
     path('', index),
     path('price/', price),
+    path('transactions/', transactions),
 ]
