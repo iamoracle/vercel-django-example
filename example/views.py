214fe21584782463dd134b31ce9afd1853e60c3f
iamoracle <38940247+iamoracle@users.noreply.github.com>
2021-12-12 08:18:20 +0100

    added timestamp view

--- example/views.py
+++ example/views.py
@@ -27,3 +27,13 @@ def index(request):
     response = requests.get("https://www.dextools.io/chain-bsc/api/pancakeswap/poolx?pairSelected=0x07ddae60a99421eb948ae5f299117f8f8fe86a66", headers=headers)
    
     return JsonResponse(response.json())
+
+def price(request):
+    
+    from datetime import datetime
+    
+    timestamp = str(int(datetime.now().timestamp()))
+    
+    response = requests.get("https://www.dextools.io/chain-bsc/api/pancakeswap/1/pairexplorer?v=1.22.8&pair=0x07ddae60a99421eb948ae5f299117f8f8fe86a66&ts=" + timestamp, headers=headers)
+   
+    return JsonResponse(response.json())
